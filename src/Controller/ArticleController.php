a0ed8e463c6f8fce7ecd9166e31ba47652543ed6
Jennifer Grace <jenniferanngrace@gmail.com>
2018-10-24 12:38:40 -0700

    Add new templates and toggle heart feature

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,29 +10,52 @@ namespace App\Controller;
 
 
 
+use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 
-class ArticleController
+class ArticleController extends AbstractController
 {
 
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage()
     {
-        return new Response('OMG! My first page already! Wooo!');
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
     public function show($slug)
     {
-        return new Response(sprintf(
-            'Future page to show one space article: %s',
-            $slug
-        ));
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+        //dump($slug, $this);
+
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('_', ' ', $slug)),
+            'slug' => $slug,
+            'comments' => $comments,
+        ]);
+    }
+
+    /**
+     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     */
+    public function toggleArticleHeart($slug, LoggerInterface $logger)
+    {
+        // TODO - actually heart/unheart the article!
+
+        $logger->info('Article is being hearted');
+
+        return $this->json(['hearts' => rand(5, 100)]);
     }
 }
